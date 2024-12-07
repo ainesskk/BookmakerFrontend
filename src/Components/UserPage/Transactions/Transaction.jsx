8b1e1c48d67960a24d68a4627d04b03a02fa44e0
ainesskk <ksenia_krr@mail.ru>
2024-12-07 03:50:30 +0300

    add transactions

--- src/Components/UserPage/Transactions/Transaction.jsx
+++ src/Components/UserPage/Transactions/Transaction.jsx
@@ -1,9 +1,26 @@
+import {useEffect, useState} from "react";
+
 export default function Transaction({transaction}){
+    const [timestamp, setTimestamp] = useState([]);
+    useEffect(() =>{
+        const resultData = transaction.dateTime.match(/(\d{4})-(\d{2})-(\d{2})\w(\d{2}:\d{2}:\d{2})/);
+        setTimestamp(resultData);
+    }, []);
     return (
         <>
-            <div>
-                <p>{transaction.amount}</p>
+            <div className="w-50 justify-content-center">
+                <div className="border border-primary p-2 rounded mb-3 d-flex justify-content-evenly align-items-center">
+                    <div className="w-50 text-center">
+                        <p className="m-0 fs-5">{`${timestamp[3]}.${timestamp[2]}.${timestamp[1]} ${timestamp[4]}`}</p>
+                    </div>
+                    <div className="w-50 text-center">
+                        <p className={transaction.type === "deposit" ? `m-0 fs-5 text-success fw-bold` : `m-0 fs-5 text-danger fw-bold`}>
+                            {transaction.type === "deposit" ? `+${transaction.amount}` : `-${transaction.amount}`}</p>
+                    </div>
+
+                </div>
             </div>
+
         </>
     )
 }
--- src/Components/UserPage/Transactions/TransactionPage.jsx
+++ src/Components/UserPage/Transactions/TransactionPage.jsx
@@ -14,11 +14,13 @@ export default function TransactionPage() {
 
     return (
         <>
-            {
-                transactions.map(transaction => {
-                   return <Transaction key={transaction.id} transaction={transaction} />
-                })
-            }
+            <div className="container w-75 mt-5 mb-5 d-flex flex-column align-items-center">
+                {
+                    transactions.map(transaction => {
+                        return <Transaction key={transaction.id} transaction={transaction}/>
+                    })
+                }
+            </div>
         </>
     )
 }
